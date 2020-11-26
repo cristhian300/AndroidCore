53c35282d1536c96f2b6b6224fb9fda97204722f
Cristhian Curiñaupa Huaman <cristhian_666win@hotmail.com>
2020-11-25 20:16:24 -0500

    actualizacion fragmentCard e iconos

---
+++ app/src/main/java/com/example/androidcore/framentos_drawer/carta/CardFragment.java
@@ -0,0 +1,24 @@
+package com.example.androidcore.framentos_drawer.carta;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.androidcore.R;
+
+
+public class CardFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_card, container, false);
+    }
+}
